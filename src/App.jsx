ffb80287cdfa252a358a141edc8637319417be81
Linsmed <iejibode@gmail.com>
2023-03-31 07:11:36 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.jsx
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import data from '../data.json';
+import Card from './components/Card';
+import Filter from './components/Filter';
+import Clear from './components/Clear';
+import Header from './components/Header';
+import Modal from './components/Modal';
+import { reduce } from 'lodash';
+
+data.map((job) => {
+  job.filters = [job.role, job.level];
+  job.filters = job.filters.concat(job.languages, job.tools);
+  return job;
+});
+
+function App() {
+  const [selectedFilters, setSelectedFilters] = useState([]);
+  const [finalJobs, setFinalJobs] = useState(data);
+  const [jobClicked, setJobClicked] = useState('');
+
+  useEffect(() => {
+    if (selectedFilters.length > 0) {
+      handleData();
+    } else {
+      setFinalJobs(data);
+    }
+  }, [selectedFilters]);
+
+  const handleFilter = (event) => {
+    if (selectedFilters.includes(event.target.value)) {
+      return null;
+    } else {
+      setSelectedFilters([...selectedFilters, event.target.value]);
+    }
+  };
+
+  const handleData = () => {
+    const filteredArrayOfObjects = reduce(
+      data,
+      (acc, job) => {
+        if (selectedFilters.every((filter) => job.filters.includes(filter))) {
+          acc.push(job);
+        }
+        return acc;
+      },
+      []
+    );
+    setFinalJobs(filteredArrayOfObjects);
+  };
+
+  const handleRemove = (event) => {
+    setSelectedFilters(
+      selectedFilters.filter((item) => item !== event.target.value)
+    );
+  };
+
+  const clearFilters = () => {
+    setSelectedFilters([]);
+  };
+
+  return (
+    <main className="font-spartan relative">
+      {jobClicked.length > 0 && (
+        <div
+          onClick={() => {
+            setJobClicked('');
+          }}
+          className="overflow-auto scrollbar-hide z-10 fixed w-screen h-screen lg:px-40 px-10 py-20 drop-shadow-2xl bg-black bg-opacity-75"
+        >
+          <div
+            onClick={(event) => {
+              event.stopPropagation();
+            }}
+            className="mx-auto max-w-7xl"
+          >
+            <div className="">
+              <Modal jobClicked={jobClicked} setJobClicked={setJobClicked} />
+            </div>
+          </div>
+        </div>
+      )}
+      <Header />
+      <section className="bg-lightCyan h-full pt-12">
+        {selectedFilters.length > 0 && (
+          <div className=" max-w-7xl mx-auto sm:px-20 px-5 -mt-14 lg:mb-0 mb-5">
+            <div className="bg-white rounded-md">
+              <div className="flex sm:justify-between justify-around px-4 py-1 items-center">
+                <div className="flex flex-wrap">
+                  {selectedFilters.map((filter, index) => (
+                    <Filter
+                      key={index}
+                      filter={filter}
+                      handleRemove={handleRemove}
+                    />
+                  ))}
+                </div>
+                <Clear clearFilters={clearFilters} />
+              </div>
+            </div>
+          </div>
+        )}
+        {finalJobs.map((jobs, index) => (
+          <Card
+            key={index}
+            jobs={jobs}
+            handleFilter={handleFilter}
+            setJobClicked={setJobClicked}
+          />
+        ))}
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        lightCyan: '#EFFAFA',
+        darkCyan: '#66a3a3',
+        veryDarkCyan: '#2C3A3A',
+      },
+      fontFamily: {
+        spartan: ['League Spartan', 'sans-serif'],
+      },
+      backgroundImage: {
+        bgHeader: "url('/images/bg-header-desktop.svg')",
+      },
+    },
+  },
+  plugins: [],
+};
